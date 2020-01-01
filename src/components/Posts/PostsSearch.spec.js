79a6709aaa4c5ace36258a1e6344202cc9294f50
Keith Richards <dkrichards86@gmail.com>
2020-01-01 18:20:35 -0500

    Updates app for text search. (#15)
    * Updates app for text search. (#15)

---
+++ src/components/Posts/PostsSearch.spec.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { createShallow } from '@material-ui/core/test-utils';
+import PostsList from "components/Posts/PostsList";
+import PostsSearch from 'components/Posts/PostsSearch';
+
+jest.mock('react-redux');
+
+jest.mock('react-router-dom', () => ({
+    useHistory: () => ({
+      push: jest.fn(),
+    }),
+}));
+
+useDispatch.mockImplementation(() => cb => cb());
+
+describe('<PostsSearch />', () => {
+    let render;
+
+    beforeAll(() => {
+        render = createShallow();
+    });
+    
+    it('renders without crashing', () => {
+        const wrapper = render(<PostsSearch />);
+        expect(wrapper.exists('[data-test-id="view-title"]'));
+        expect(wrapper.exists('[data-test-id="query-input"]'));
+        expect(wrapper.exists('[data-test-id="clear-button"]'));
+        expect(wrapper.exists('[data-test-id="submit-button"]'));
+        expect(wrapper.contains(<PostsList />)).toBe(false);
+    });
+
+    it('submits a valid form', () => {
+        const wrapper = render(<PostsSearch />);
+        wrapper.find('[data-test-id="query-input"]').prop('onChange')({target:{value: 'foobar'}});
+        wrapper.find('[data-test-id="submit-button"]').simulate('click');
+
+        expect(wrapper.find('[data-test-id="query-input"]').props().value).toEqual('foobar');
+        expect(wrapper.find('[data-test-id="query-input"]').props().error).toBeFalsy();
+        expect(wrapper.find('[data-test-id="query-input"]').props().helperText).toBeFalsy();
+    });
+
+    it('shows errors on empty inputs', () => {
+        const wrapper = render(<PostsSearch />);
+        wrapper.find('[data-test-id="query-input"]').prop('onChange')({target:{value: ''}});
+        wrapper.find('[data-test-id="submit-button"]').simulate('click');
+
+        expect(wrapper.find('[data-test-id="query-input"]').props().value).toEqual('');
+        expect(wrapper.find('[data-test-id="query-input"]').props().error).toBeTruthy();
+        expect(wrapper.find('[data-test-id="query-input"]').props().helperText).toEqual('This field is required.');
+
+        expect(wrapper.contains(<PostsList />)).toBe(false);
+    });
+});
